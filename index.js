b0cf0ff7dad217c05757b68c1e8138c5dfb237a6
Lara <laraellenh@gmail.com>
2022-01-12 11:59:36 -0500

    commit tree 2fc59506e89dcccdb3bd7ed347b5cb77a237e413

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 
+const thankYouNotes = ["Thank you, Lisa, for the wonderful surprise gift!", "Thank you, Kaitlin, for the wonderful surprise gift!",
+"Thank you, Jan, for the wonderful surprise gift!"]
 
- function writeCards(names, event) {
-     let messages = [];
-     for(let i = 0; i < names.length; i++) {
-        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+function writeCards(cards) {
+    for (let i =0; i<cards.length; i++) {
+        console.log(`wrote ${cards[i]} and mailed them`)
     }
-     return messages;
-
- }
-
- function countDown(positiveInteger) {
-while(positiveInteger >=0) {
-    console.log(positiveInteger);
-    positiveInteger--;
+    return thankYouNotes
 }
- }
+writeCards(cards);
+
+
+function countDown(number) {
+    // the number should be an integer greater than 0
+    while (number > 0 ) {
+        // ivokes console log once for each number until 0
+        console.log(number);
+        number -= 1;
+    }
+    // logs each number until 0
+    console.log(number);
+    }
+// invoke the function using the number para to enable the countdown
+    countDown(number);
